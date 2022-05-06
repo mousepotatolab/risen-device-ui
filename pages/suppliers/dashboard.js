b2d0b7667afc8cedc0b4d92104ee57a9cfd8321a
Emrul <emrul7427@gmail.com>
2022-05-06 11:13:00 +0600

    supplier panel

---
+++ pages/suppliers/dashboard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// components
+
+import CardLineChart from "components/Cards/CardLineChart.js";
+import CardBarChart from "components/Cards/CardBarChart.js";
+import CardPageVisits from "components/Cards/CardPageVisits.js";
+import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+
+// layout for page
+
+import Admin from "layouts/Admin.js";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        
+      </div>
+    </>
+  );
+}
+
+Dashboard.layout = Admin;
